d8b487c92af06d19f91d62e0441a085ad4ab7997
ckkoch <el.toffo@gmx.net>
2016-05-12 14:02:55 +0200

    work on resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// resumee starts here
+
+var name = "Christoph Koch"
+var role = "Captain"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
