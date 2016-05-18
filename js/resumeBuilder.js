6ee53d84bf7449c1e4f18479e723e4b62befdef2
ckkoch <el.toffo@gmx.net>
2016-05-18 13:34:40 +0200

    prepare objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,3 +11,20 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var bio = {
+    "name" : "Christoph",
+    "role" : "Captain",
+    "contactInfo" : {"mobile":"123-456-789",
+                   "email":"test@example.com"
+    },
+    "bioPic" : "images/fry.jpg",
+    "welcomeMessage" : "Hi there!",
+    "skills" : ["reading", "riting", "rithmetik"]
+};
+
+var work = {};
+var projects = {};
+var education = {};
+
+$("#main").append(bio.name);
